1f9c5fe51c6db67989cd5fbd0ba76a2950ff4626
Justin Dave <jdahiroc_210000000608@uic.edu.ph>
2024-04-16 03:54:32 +0800

    added homepage design for user

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,9 @@
 // img
 import logoBrand from "../assets/LOGOBRAND.png";
 import userIcon from "../assets/MaleUser.png";
+import bannerImg from "../assets/banner.png";
+import headerLine from "../assets/headerLine.png";
+import courseLogo from "../assets/CourseLogo.png";
 
 //css
 import "../pages/homepage.css";
@@ -26,8 +29,104 @@ const Homepage = () => {
             <img src={userIcon} alt="profile-icon" />
           </div>
           <div className="profile-name-container">
-            <h4>Admin 01</h4>
+            <h4>Admin</h4>
           </div>
+        </div>
+      </div>
+
+      {/* Banner */}
+      <div className="banner-container">
+        <div className="banner-img">
+          <img src={bannerImg} alt="LMS Banner" />
+        </div>
+      </div>
+
+      {/* Header */}
+      <div className="header-container">
+        <h2>Courses</h2>
+      </div>
+      <div className="headerLine-container">
+        <img src={headerLine} alt="headerLine" />
+      </div>
+
+      {/* Course Tiles */}
+      <div className="box">
+        <div className="img-course-container">
+          <img src={courseLogo} alt="Course Logo" />
+        </div>
+        <div className="course-user-container">
+          <span>John Mike S. Wayne</span>
+        </div>
+        <div className="course-title-container">
+          <span>Health Course 1</span>
+        </div>
+        <div className="course-description-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus animi natus maiores obcaecati incidunt dolore
+            dignissimos mollitia adipisci amet optio. Voluptas, deleniti!
+            Voluptatum repellat maiores temporibus, earum nihil non eos.
+          </p>
+        </div>
+      </div>
+
+      <div className="box2">
+        <div className="img-course-container">
+          <img src={courseLogo} alt="Course Logo" />
+        </div>
+        <div className="course-user-container">
+          <span>John Mike S. Wayne</span>
+        </div>
+        <div className="course-title-container">
+          <span>Health Course 1</span>
+        </div>
+        <div className="course-description-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus animi natus maiores obcaecati incidunt dolore
+            dignissimos mollitia adipisci amet optio. Voluptas, deleniti!
+            Voluptatum repellat maiores temporibus, earum nihil non eos.
+          </p>
+        </div>
+      </div>
+
+      <div className="box3">
+        <div className="img-course-container">
+          <img src={courseLogo} alt="Course Logo" />
+        </div>
+        <div className="course-user-container">
+          <span>John Mike S. Wayne</span>
+        </div>
+        <div className="course-title-container">
+          <span>Health Course 1</span>
+        </div>
+        <div className="course-description-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus animi natus maiores obcaecati incidunt dolore
+            dignissimos mollitia adipisci amet optio. Voluptas, deleniti!
+            Voluptatum repellat maiores temporibus, earum nihil non eos.
+          </p>
+        </div>
+      </div>
+
+      <div className="box4">
+        <div className="img-course-container">
+          <img src={courseLogo} alt="Course Logo" />
+        </div>
+        <div className="course-user-container">
+          <span>John Mike S. Wayne</span>
+        </div>
+        <div className="course-title-container">
+          <span>Health Course 1</span>
+        </div>
+        <div className="course-description-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus animi natus maiores obcaecati incidunt dolore
+            dignissimos mollitia adipisci amet optio. Voluptas, deleniti!
+            Voluptatum repellat maiores temporibus, earum nihil non eos.
+          </p>
         </div>
       </div>
     </>
