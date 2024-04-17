2a1908a599b8b1175adfc2901c9d09b720695372
Justin Dave <jdahiroc_210000000608@uic.edu.ph>
2024-04-18 01:33:46 +0800

    adjust the design

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -7,7 +7,6 @@ import "../styles/signIn.css";
 
 // img
 import loginImg from "../assets/signinImage.png";
-// import SignUp from "../pages/SignUp";
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -33,27 +32,33 @@ const SignIn = () => {
     <>
       <div className="error-container-login">
         {/* error message */}
-        <p className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
+        <p className={errMsg ? "errmsg" : ""} aria-live="assertive">
           {errMsg}
         </p>
       </div>
-      <div className="container">
-        <div className="side-img">
+      <div className="signIn-container">
+        {/* image */}
+        <div className="side-img-container">
           <img src={loginImg} alt="side-img" />
         </div>
-        <div className="header">
-          <div className="text">
-            SIGN IN
-            <div className="text">SIGN UP</div>
+        {/* header */}
+        <div className="header-text-container">
+          <div className="sign-in-container">
+            <h2>SIGN IN</h2>
+          </div>
+          <div className="sign-up-container">
+            <h2>SIGN UP</h2>
           </div>
           <div className="underline"></div>
         </div>
-        <div className="inputs">
+
+        {/* inputs */}
+        <div className="inputs-container">
           <form onSubmit={handleSubmit}>
             <div className="input">
               <input
                 type="text"
-                placeholder="Username"
+                placeholder="USERNAME"
                 autoComplete="off"
                 onChange={(e) => setEmail(e.target.value)}
               />
@@ -61,21 +66,21 @@ const SignIn = () => {
             <div className="input">
               <input
                 type="password"
-                placeholder="Password"
+                placeholder="PASSWORD"
                 autoComplete="off"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div className="sign-up-nav">
               <p>
-                Don&#39;t have an account?{" "}
+                Don&#39;t have an account?
                 <Link className="sign-up-link" to={"/signup"}>
                   Sign up.
                 </Link>
               </p>
             </div>
             <div className="submit-container">
-              <button className="submit">SIGN IN</button>
+              <button className="submit">Login</button>
             </div>
           </form>
         </div>
