dcbda89371cb3ab3f372e93e96e7d698bfbe46ff
Justin Dave <jdahiroc_210000000608@uic.edu.ph>
2024-04-21 20:39:27 +0800

    added protected routes for Hompage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 
-// components
+// Components
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Homepage from "./pages/Homepage";
+import ProtectedRoute from "../src/ProtectedRoutes/ProtectedRoute";
 
-
-
+// Admin components
+// TODO add components for admin pages
 
 function App() {
   return (
@@ -17,7 +18,19 @@ function App() {
           <Routes>
             <Route path="/" index element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/homepage" element={<Homepage />} />
+            
+            {/* User Routes */}
+            {/* Normal User Homepage */}
+            <Route
+              path="/homepage"
+              element={
+                <ProtectedRoute>
+                  <Homepage />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Admin Routes */}
           </Routes>
         </AuthContextProvider>
       </div>
