5e557cd5add7b68e515af4ccddfdc3ba51626977
Justin Dave <jdahiroc_210000000608@uic.edu.ph>
2024-05-05 22:26:38 +0800

    Added Sign In function determines Teacher or Student

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,26 +1,28 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { UserAuth } from "../context/AuthContext";
-import { getDocs, collection, query, where } from "firebase/firestore"; // Import Firestore functions
+import { getDocs, collection, query, where } from "firebase/firestore";
 import { db } from "../firebase";
 
-// CSS
 import "../styles/signIn.css";
-
-// img
 import loginImg from "../assets/signinImage.png";
 
 const SignIn = () => {
   const navigate = useNavigate();
-  const { signIn, updateProfile } = UserAuth();
+  const { signIn } = UserAuth();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errMsg, setErrMsg] = useState("");
 
-  // Function to fetch full name from Firestore based on email
-  const fetchFullNameByEmail = async (email) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrMsg("");
     try {
+      // Reference for email/password to authenticate user account
+      await signIn(email, password);
+
+      // Fetch user data from Firestore based on email
       const usersRef = collection(db, "users");
       const q = query(usersRef, where("email", "==", email));
       const querySnapshot = await getDocs(q);
@@ -31,49 +33,30 @@ const SignIn = () => {
 
       // Assuming there's only one user with the given email
       const userData = querySnapshot.docs[0].data();
-      return userData.fName;
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-      throw error;
-    }
-  };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setErrMsg("");
-    try {
-      // Reference for email/password to authenticate user account
-      await signIn(email, password);
-
-      // Fetch full name from Firestore based on email
-      const fName = await fetchFullNameByEmail(email);
-
-      // Update user profile with full name
-      await updateProfile(fName);
-
-      // if authentication is successful navigate user to homepage
-      // but based on user role
-      navigate("/home");
+      // Redirect based on user type
+      if (userData.userType === "Student") {
+        navigate("/home");
+      } else if (userData.userType === "Teacher") {
+        navigate("/admin/home");
+      }
     } catch (e) {
       console.log(e.message);
-      return setErrMsg(e.message);
+      setErrMsg(e.message);
     }
   };
 
   return (
     <>
       <div className="error-container-login">
-        {/* error message */}
         <p className={errMsg ? "errmsg" : ""} aria-live="assertive">
           {errMsg}
         </p>
       </div>
       <div className="signIn-container">
-        {/* image */}
         <div className="side-img-container">
           <img src={loginImg} alt="side-img" />
         </div>
-        {/* header */}
         <div className="header-text-container">
           <div className="sign-in-container">
             <h2>SIGN IN</h2>
@@ -83,8 +66,6 @@ const SignIn = () => {
           </div>
           <div className="underline"></div>
         </div>
-
-        {/* inputs */}
         <div className="inputs-container">
           <form onSubmit={handleSubmit}>
             <div className="input">
