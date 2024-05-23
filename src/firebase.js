4023f9eb3558b0cb95ce9d7a78918ef643536dcb
jdahiroc <jdahiroc_210000000608@uic.edu.ph>
2024-05-23 16:50:50 +0800

    added firebase storage

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDMbPhApQJd149b20cVAEUEA5jorrBwKWY",
   authDomain: "learning-management-syst-d83de.firebaseapp.com",
@@ -19,4 +17,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export default app;
