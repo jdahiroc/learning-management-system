262f30dec589bed8e6005adb919b118aee850bc9
jdahiroc <jdahiroc_210000000608@uic.edu.ph>
2024-05-25 22:59:40 +0800

    can display the materials and assessments

---
+++ src/pages/AssessmentPage.jsx
@@ -0,0 +1,166 @@
+// img
+import logoBrand from "../assets/LOGOBRAND.png";
+import userIcon from "../assets/profileIcon.png";
+import profileIcon from "../assets/profileIcon.png";
+import LessonLogo from "../assets/Lessons.png";
+import thumbnail1 from "../assets/thumbnail1.png";
+
+// React Hooks
+import { useState, useEffect } from "react";
+import { useNavigate, Link, useParams } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+//css
+import "../styles/assessmentPage.css";
+
+// Firebase
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+
+const CourseSection2 = () => {
+  const [profileModal, setProfileModal] = useState(false);
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
+  const { courseId, assessmentId } = useParams();
+
+  const [assessment, setAssessment] = useState(null); // State to store assessment data
+
+  // Function to fetch assessment data
+  const fetchAssessmentData = async () => {
+    try {
+      if (user && user.uid && assessmentId) {
+        const assessmentDocRef = doc(
+          db,
+          `Courses/${user.uid}/UserCourses/${courseId}/Assessments`,
+          assessmentId
+        );
+        const assessmentDocSnap = await getDoc(assessmentDocRef);
+        if (assessmentDocSnap.exists()) {
+          setAssessment(assessmentDocSnap.data());
+        } else {
+          console.log("No such assessment document!");
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching assessment data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAssessmentData();
+  }, [user, courseId, assessmentId]);
+
+  //Profile Modal Function
+  const toggleProfileModal = () => {
+    setProfileModal(!profileModal);
+  };
+
+  //logout function
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+      console.log("You are logged out!");
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  return (
+    <>
+      {/* NAVIGATIONS */}
+      <div className="navigation-container">
+        {/* nav-logo */}
+        <div className="logo-container">
+          <Link to="/admin/home">
+            <img src={logoBrand} alt="logoBrand" loading="lazy" />
+          </Link>
+        </div>
+        <div className="navigation-controls">
+          <ul>
+            <Link to="/admin/page/course">
+              <li>COURSES</li>
+            </Link>
+            <li>ABOUT US</li>
+            <li>CONTACT US</li>
+          </ul>
+        </div>
+        {/* Navigation Profile */}
+        <div onClick={toggleProfileModal} className="profile-container">
+          <div className="profile-icon-container">
+            <img src={userIcon} alt="profile-icon" />
+          </div>
+          <div className="profile-name-container">
+            <h4>{user && user.displayName}!</h4>
+          </div>
+        </div>
+      </div>
+
+      {/* Profile Modal */}
+      <div className={`overlay-profileIcon  ${profileModal ? "show" : ""}`}>
+        <div className="close-btn-container">
+          <button className="profile-closebtn" onClick={toggleProfileModal}>
+            &times;
+          </button>
+        </div>
+        <div className="profileIcon-modal-container">
+          <div className="profile-icon-container">
+            <img src={profileIcon} alt="profile-icon" />
+          </div>
+          <div className="userName-container">
+            <h2>Hi, {user && user.displayName}!</h2>
+          </div>
+          <div className="logout-container">
+            <button onClick={handleLogout} className="logout-btn">
+              LOGOUT
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="course2-container">
+        {assessment ? (
+          <div className="course2-header">
+            <h3 className="course2-title">{assessment.assessmentTitle}</h3>
+            <div className="course2-underline"></div>
+
+            <div className="content-course2">
+              <img src={LessonLogo} alt="logoBrand" className="course2-logo1" />
+
+              <h3 className="c2-lesson-title">{assessment.assessmentTitle}</h3>
+              <h3 className="c2-lesson-date">
+                {assessment.addedOn &&
+                  new Date(assessment.addedOn.toDate()).toLocaleDateString()}
+              </h3>
+
+              <div className="course2-underline2"></div>
+
+              <div className="c2-lessons-content">
+                <div className="c2-lessons1">
+                  <img
+                    src={thumbnail1}
+                    className="thumbnail1-img"
+                    alt="thumbnail"
+                  />
+                  <h3 className="c2-lesson1-title">
+                    {assessment.assessmentTitle}
+                  </h3>
+                  <h3 className="c2-lesson1-description">
+                    {assessment.assessmentInstructions}
+                  </h3>
+                  <h3 className="c2-lesson1-file">{assessment.attachments}</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p>Loading assessment data...</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default CourseSection2;
---
+++ src/pages/LessonMaterialsPage.jsx
@@ -0,0 +1,167 @@
+// React Imports
+import { useState, useEffect } from "react";
+import { useNavigate, Link, useParams } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+// CSS
+import "../styles/lessonmaterialsPage.css";
+
+// Images
+import logoBrand from "../assets/LOGOBRAND.png";
+import userIcon from "../assets/profileIcon.png"; // Changed from profileIcon to userIcon
+import LessonLogo from "../assets/Lessons.png";
+import thumbnail1 from "../assets/thumbnail1.png";
+
+// Firebase
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+
+const LessonMaterialsPage = () => {
+  const [profileModal, setProfileModal] = useState(false);
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
+  const { courseId, lessonId } = useParams();
+
+  const [lesson, setLesson] = useState(null); // State to store lesson data
+
+  // Function to fetch lesson data
+  const fetchLessonData = async () => {
+    try {
+      if (user && user.uid && lessonId) {
+        const lessonDocRef = doc(
+          db,
+          `Courses/${user.uid}/UserCourses/${courseId}/lessonMaterials`,
+          lessonId
+        );
+        const lessonDocSnap = await getDoc(lessonDocRef);
+        if (lessonDocSnap.exists()) {
+          setLesson(lessonDocSnap.data());
+        } else {
+          console.log("No such lesson document!");
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching lesson data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLessonData(); // Fetch lesson data when component mounts
+  }, [user, courseId, lessonId]); // Trigger fetch when user, courseId, or lessonId changes
+
+  // Toggle Profile Modal Function
+  const toggleProfileModal = () => {
+    setProfileModal(!profileModal);
+  };
+
+  // Logout Function
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+      console.log("You are logged out!");
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  return (
+    <>
+      {/* Navigation */}
+      <div className="navigation-container">
+        {/* Logo */}
+        <div className="logo-container">
+          <Link to="/admin/home">
+            <img src={logoBrand} alt="logoBrand" loading="lazy" />
+          </Link>
+        </div>
+        {/* Navigation Controls */}
+        <div className="navigation-controls">
+          <ul>
+            <Link to="/admin/page/course">
+              <li>COURSES</li>
+            </Link>
+            <li>ABOUT US</li>
+            <li>CONTACT US</li>
+          </ul>
+        </div>
+        {/* Profile Icon */}
+        <div onClick={toggleProfileModal} className="profile-container">
+          <div className="profile-icon-container">
+            <img src={userIcon} alt="profile-icon" />
+          </div>
+          <div className="profile-name-container">
+            <h4>{user && user.displayName}</h4>
+          </div>
+        </div>
+      </div>
+
+      {/* Profile Modal */}
+      <div className={`overlay-profileIcon ${profileModal ? "show" : ""}`}>
+        <div className="close-btn-container">
+          <button className="profile-closebtn" onClick={toggleProfileModal}>
+            &times;
+          </button>
+        </div>
+        <div className="profileIcon-modal-container">
+          <div className="profile-icon-container">
+            <img src={userIcon} alt="profile-icon" />
+          </div>
+          <div className="userName-container">
+            <h2>Hi, {user && user.displayName}!</h2>
+          </div>
+          <div className="logout-container">
+            <button onClick={handleLogout} className="logout-btn">
+              LOGOUT
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Course Content */}
+      <div className="course2-container">
+        {lesson ? (
+          <div className="course2-header">
+            <h3 className="course2-title">{lesson.lessonTitle}</h3>
+            <div className="course2-underline"></div>
+
+            <div className="content-course2">
+              <img src={LessonLogo} alt="logoBrand" className="course2-logo1" />
+
+              <h3 className="c2-lesson-title">{lesson.lessonTitle}</h3>
+              <h3 className="c2-lesson-date">
+                {lesson.addedOn &&
+                  new Date(lesson.addedOn.toDate()).toLocaleDateString()}
+              </h3>
+
+              <div className="course2-underline2"></div>
+
+              <div className="c2-lessons-content">
+                <div className="c2-lessons1">
+                  <img
+                    src={thumbnail1}
+                    className="thumbnail1-img"
+                    alt="thumbnail"
+                  />
+                  <h3 className="c2-lesson1-title">{lesson.lessonTitle}</h3>
+                  <h3 className="c2-lesson1-description">
+                    {lesson.lessonDescription}
+                  </h3>
+                  <h3 className="c2-lesson1-file">
+                    {lesson.lessonAttachments}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p>No lesson data available.</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default LessonMaterialsPage;
